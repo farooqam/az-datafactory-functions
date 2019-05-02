780fada9fc61e91757f30639d80f9cda5c6e2041
Farooq Mahmud <farooq.mahmud@marel.com>
2019-05-02 11:07:53 -0700

    Initial commit.

---
+++ HelloWorld/HelloWorldFunctionApp/HelloWorldFunction.cs
@@ -0,0 +1,33 @@
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace HelloWorldFunctionApp
+{
+    public static class HelloWorldFunction
+    {
+        [FunctionName("FarooqHelloWorld")]
+        public static async Task<JObject> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            return name != null
+                ? JObject.FromObject(new {message = $"Hello {name}!"})
+                : JObject.FromObject(new {error = "Specify a name."});
+        }
+    }
+}
